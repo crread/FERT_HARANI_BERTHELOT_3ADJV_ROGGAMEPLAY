47cde856cda326d7e4600c19ce1780886d0c2222
unknown <mickaelberthelot@outlook.fr>
2021-03-23 19:20:50 +0100

    test save

--- Assets/Scripts/Components/Life.cs
+++
@@ -1,6 +0,0 @@
-using UnityEngine;
-
-public class Life : GenericComponent
-{ //Module HMov
-    int health = 100;
-}
--- Assets/Scripts/Components/MoveSinus.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-public class MoveSinus : GenericComponent
-{ //Module VMov
-   
-   private float speed = 1;
-}
