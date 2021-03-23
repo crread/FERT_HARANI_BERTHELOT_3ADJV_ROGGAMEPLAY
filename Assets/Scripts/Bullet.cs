6478ee13c4e6ba737f172a58de71e154d68f315f
quent <quentin2000.fert@gmail.com>
2021-03-23 16:53:32 +0100

    Revert "Revert "Merge branch 'sam_dev' into Quentin""
    This reverts commit 264e4b086fe2e607efc9907cc4ea07b76bd84fcb.

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    
+    //variables
+    public float speed;
+    public float maxDistance;
+
+    private GameObject triggeringEnemy;
+    public float damage;
+
+    private GameObject player;
+    
+    //Méthodes
+    private void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+    }
+
+    private void Update()
+    {
+        transform.Translate(Vector3.forward*Time.deltaTime*speed);
+        maxDistance += 1 * Time.deltaTime;
+        if (maxDistance>= 5)
+            Destroy(this.gameObject);
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Enemy")
+        {
+            triggeringEnemy = other.gameObject;
+            triggeringEnemy.GetComponent<Enemy>().health -= damage;
+            Destroy(this.gameObject);
+        }
+
+        if (other.tag == "Player")
+        {
+            player.GetComponent<Player>().health -= 20;
+        }
+    }
+}
