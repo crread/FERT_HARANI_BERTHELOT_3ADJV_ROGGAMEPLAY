9c1fe90efa9d411c730b0314a786cdb2459a9016
sharani97 <73841513+sharani97@users.noreply.github.com>
2021-03-23 14:14:41 +0100

    Add files via upload

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+   // Variables
+   public Transform player;
+   public float smooth = 0.3f;
+
+   private Vector3 velocity = Vector3.zero;
+   
+   
+   // méthodes
+
+   void Update()
+   {
+      Vector3 pos = new Vector3();
+      pos.x = player.position.x;
+      pos.z = player.position.y - 7f;
+      pos.y = player.position.z;
+      transform.position = Vector3.SmoothDamp(transform.position, pos, ref velocity, smooth);
+   }
+}
---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    //Variables
+    public float health; 
+    public float pointsToGive; 
+    public GameObject player; 
+    public float waitTime; 
+    private float currentTime; 
+    private bool shot; 
+    public GameObject bullet; 
+    public GameObject bulletSpawnPoint; 
+    private Transform bulletSpawned;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player"); 
+        bulletSpawnPoint = GameObject.Find("SpawnPoint"); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(health <= 0) 
+        { 
+            Die();
+        } 
+        //this.transform.LookAt(player.transform); 
+
+        if(currentTime == 0)
+            Shoot();
+
+        if(shot && currentTime<waitTime)
+            currentTime+=1*Time.deltaTime;
+
+        if(currentTime >= waitTime)
+            currentTime = 0;
+    }
+
+    public void Die()
+    {
+        Destroy(this.gameObject);
+        player.GetComponent<Player>().points += pointsToGive;
+    }
+
+    public void Shoot()
+    {
+        shot = true;
+        bulletSpawned = Instantiate(bullet.transform, bulletSpawnPoint.transform.position, Quaternion.identity);
+        bulletSpawned.rotation = this.transform.rotation;
+        
+    }
+}
